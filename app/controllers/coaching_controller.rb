789c6838ac11f9723219ba6fac1476a9ee8c561c
Vincent Henry-Roger <v.henryroger@gmail.com>
2017-07-10 16:27:18 +0200

    TentativeExo1/stupidCoaching

---
+++ app/controllers/coaching_controller.rb
@@ -0,0 +1,34 @@
+class CoachingController < ApplicationController
+  def answer
+    @query = params[:query]
+    @response = coach_answer(@query)
+  end
+
+  def ask
+  end
+
+private
+
+  def coach_answer(your_message)
+    if your_message.downcase == "i am going to work right now!"
+      ''
+    elsif your_message.end_with?("?")
+      "Silly question, get dressed and go to work!"
+    else
+      "I don't care, get dressed and go to work!"
+    end
+  end
+
+  def coach_answer_enhanced(your_message)
+    answer = coach_answer(your_message)
+    if answer != ""
+      if your_message.upcase == your_message
+        "I can feel your motivation! #{answer}"
+      else
+        answer
+      end
+    else
+      ""
+    end
+  end
+end
